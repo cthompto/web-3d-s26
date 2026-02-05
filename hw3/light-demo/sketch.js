f0a29d8346b25f9ce817005644e8a932b2e789fc
Chelsea Thompto <chelsea.l.thompto@gmail.com>
2026-02-05 15:21:55 -0500

    add demo templates for today

---
+++ hw3/light-demo/sketch.js
@@ -0,0 +1,47 @@
+let myShape;
+
+
+function setup() {
+    let canvas = createCanvas(400, 400, WEBGL);
+    angleMode(DEGREES);
+    //ballBlob();
+}
+
+function draw() {
+    background(0);
+    orbitControl();
+    noStroke();
+    lights();
+    fill(0,255,255);
+    shininess(10);
+    specularMaterial(255,0,255);
+    //emissiveMaterial(255,0,255);
+    ellipsoid(100,50,25);
+    //filter(POSTERIZE, 1);
+    //model(myShape);
+    
+}
+
+// demo for proceedural shapes
+function ballBlob() {
+    beginGeometry();
+    for (let i = 0; i < 200; i++) {
+        let r = random(255);
+        let g = random(255);
+        let b = random(255);
+        fill(r, g, b);
+        let x = random(-100, 100);
+        let y = random(-100, 100);
+        let z = random(-100, 100);
+        let size = random(5, 20);
+        push();
+        translate(x, y, z);
+        sphere(size);
+        translate(0,-size,0);
+        sphere(size*0.75);
+        translate(0,-size*0.75,0);
+        sphere(size*0.5);
+        pop();
+    }
+    myShape = endGeometry();
+}
