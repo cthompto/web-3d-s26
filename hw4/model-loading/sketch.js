fb54755b1f1c3c7665f5ddbab8e917ae7df4cc4c
Chelsea Thompto <chelsea.l.thompto@gmail.com>
2026-02-10 10:33:24 -0500

    add model loading

---
+++ hw4/model-loading/sketch.js
@@ -0,0 +1,22 @@
+let myShape;
+function preload() {
+  myShape = loadModel('./fog_block_1.obj', true);
+}
+
+function setup() {
+    let canvas = createCanvas(400, 400, WEBGL);
+    canvas.parent("p5-holder"); 
+    angleMode(DEGREES);
+}
+
+function draw() {
+    background(255);
+    orbitControl();
+    noStroke();
+    lights();
+    fill(0,255,255);
+    shininess(10);
+    specularMaterial(255,0,255);
+    rotateX(180);  
+    model(myShape);
+}
