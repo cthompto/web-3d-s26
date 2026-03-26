f2325f6d44013469d35c9bc56ebf6749179ca7c7
Chelsea Thompto <chelsea.l.thompto@gmail.com>
2026-03-25 20:58:50 -0400

    image demo

--- image-fonts/script.js
+++ image-fonts/script.js
@@ -30,9 +30,6 @@ let prevTime = performance.now();
 const velocity = new THREE.Vector3();
 const direction = new THREE.Vector3();
 
-// march 24 variables 
-let torusLine;
-
 // Run the "init" function which is like "setup" in p5.
 init();
 
@@ -165,61 +162,43 @@ function init() {
         });
 
         // create message
-        const message = "March 24th Demo";
-        
+        const message = "Image Demo";
+
         // generate text shapes
-        const shapes = font.generateShapes(message, 30);
+        const shapes = font.generateShapes(message, 10);
         const fontGeo = new THREE.ShapeGeometry(shapes);
         fontGeo.computeBoundingBox();
-        
+
         // center alignment
         const xMid = -0.5 * (fontGeo.boundingBox.max.x - fontGeo.boundingBox.min.x);
         fontGeo.translate(xMid, 0, 0);
 
         // add to scene
         const text = new THREE.Mesh(fontGeo, matLite);
-        text.position.z = -200;
-        text.position.y = 50;
+        text.position.z = -100;
+        text.position.y = 30;
         scene.add(text);
     });
 
     // Add world geometry
 
-    // March 24 example material and object
-    
-    // donut shape
-    const donut = new THREE.TorusGeometry( 50, 20, 16, 100 );
-    
-    // donut solid color
-    const donutMaterial = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
-    const torus = new THREE.Mesh( donut, donutMaterial );
-    torus.position.z = -250;
-    torus.position.y = 50;
-    scene.add( torus );
-    
-    // donut line color 
-    // this one looks a little different due to the animation
-    // because i needed to call the variable in another function
-    // i declared the variable at the top of the sketch
-    const donutLine = new THREE.MeshBasicMaterial( { color: 0x000000, wireframe: true } );
-    torusLine = new THREE.Mesh( donut, donutLine ); 
-    torusLine.position.z = -250;
-    torusLine.position.y = 50;
-    scene.add( torusLine );
-    
-    // torus knot shape
-    const coolShape = new THREE.TorusKnotGeometry( 10, 3, 100, 16 );
-    
-    // torus knot color
-    const knotColor = new THREE.MeshPhongMaterial( {color: 0x7f01a2} );
-    const knotShape = new THREE.Mesh( coolShape, knotColor );
-    knotShape.position.z = -300;
-    knotShape.position.y = 50;
-    scene.add( knotShape );
+    // image
 
+    const imgSource = new THREE.TextureLoader().load("./cab-curio-1.jpg");
+    const imgMaterial = new THREE.MeshBasicMaterial({
+        map: imgSource,
+        side: THREE.DoubleSide,
+        transparent: true
+    });
+    const imgGeometry = new THREE.PlaneGeometry(200, 150);
+    const imgPlane = new THREE.Mesh(imgGeometry, imgMaterial);
+    imgPlane.position.set(0, 25, -200);
+    //planeI5.rotation.set(1.5708, 0, 0);
+    scene.add(imgPlane);
+    
     // Ground
     const earth = new THREE.PlaneGeometry(4000, 4000);
-    const ground = new THREE.MeshPhongMaterial({ color: 0x402314, flatShading: true });
+    const ground = new THREE.MeshPhongMaterial({ color: 0xe10dee, flatShading: true });
     const mesh2 = new THREE.InstancedMesh(earth, ground, 500);
     mesh2.translateY(-60);
     mesh2.rotateX(-1.5708);
@@ -274,8 +253,6 @@ function animate() {
     prevTime = time;
     // End First Person Control Animations
 
-    torusLine.rotation.z += 0.001;
-    
     render();
 }
 
