f8c1608d05b04898aa6e643c729c51cfa53b5d26
Chelsea Thompto <chelsea.l.thompto@gmail.com>
2026-04-02 17:17:36 -0400

    adv. material demo start

---
+++ three-adv-material/script.js
@@ -0,0 +1,296 @@
+// Video Example Three.js Example
+// Chelsea Thompto - Spring 2026
+
+// Three.js uses an import map to add features.
+// The "import * as THREE from 'three';" will be
+// in all sketches. Add-ons will be added after.
+
+// The main library script
+import * as THREE from "three";
+
+// The plug-ins
+import { PointerLockControls } from "../src/PointerLockControls.js";
+import { Font } from "../src/FontLoader.js";
+import { TTFLoader } from "../src/TTFLoader.js";
+import { TextGeometry } from "../src/TextGeometry.js";
+
+// Declaring global variables.
+let camera, canvas, controls, scene, renderer;
+
+// Variables for First Person Controls
+let raycaster;
+let moveForward = false;
+let moveBackward = false;
+let moveLeft = false;
+let moveRight = false;
+let canJump = true;
+let prevTime = performance.now();
+const velocity = new THREE.Vector3();
+const direction = new THREE.Vector3();
+
+// Variables for scene objects
+let font;
+let text = "Video Demo";
+let textGeo;
+let materials;
+let textMesh1;
+let textMesh2;
+let group;
+let video;
+let vidTexture;
+
+// Run the "init" function which is like "setup" in p5.
+init();
+
+// Define initial scene
+function init() {
+    // scene setup
+    canvas = document.getElementById("3-holder");
+    scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xbfeff5);
+    scene.fog = new THREE.FogExp2(0xbfeff5, 0.0015);
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    //renderer.setPixelRatio( window.devicePixelRatio ); 
+    renderer.setSize(innerWidth, innerHeight);
+    renderer.setAnimationLoop(animate);
+    canvas.appendChild(renderer.domElement);
+
+    // Setup camera
+    camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 1000);
+    camera.position.set(0, 10, 0);
+
+    // Setup First Person Controls
+    // DO NOT TOUCH
+
+    controls = new PointerLockControls(camera, document.body);
+
+    const blocker = document.getElementById("blocker");
+    const instructions = document.getElementById("instructions");
+
+    instructions.addEventListener("click", function () {
+        controls.lock();
+        video.play();
+    });
+
+    controls.addEventListener("lock", function () {
+        instructions.style.display = "none";
+        blocker.style.display = "none";
+    });
+
+    controls.addEventListener("unlock", function () {
+        blocker.style.display = "block";
+        instructions.style.display = "";
+    });
+
+    scene.add(controls.object);
+
+    const onKeyDown = function (event) {
+        switch (event.code) {
+            case "ArrowUp":
+            case "KeyW":
+                moveForward = true;
+                break;
+
+            case "ArrowLeft":
+            case "KeyA":
+                moveLeft = true;
+                break;
+
+            case "ArrowDown":
+            case "KeyS":
+                moveBackward = true;
+                break;
+
+            case "ArrowRight":
+            case "KeyD":
+                moveRight = true;
+                break;
+
+            case "Space":
+                if (canJump === true) velocity.y += 350;
+                canJump = false;
+                break;
+        }
+    };
+
+    const onKeyUp = function (event) {
+        switch (event.code) {
+            case "ArrowUp":
+            case "KeyW":
+                moveForward = false;
+                break;
+
+            case "ArrowLeft":
+            case "KeyA":
+                moveLeft = false;
+                break;
+
+            case "ArrowDown":
+            case "KeyS":
+                moveBackward = false;
+                break;
+
+            case "ArrowRight":
+            case "KeyD":
+                moveRight = false;
+                break;
+        }
+    };
+
+    document.addEventListener("keydown", onKeyDown);
+    document.addEventListener("keyup", onKeyUp);
+
+    raycaster = new THREE.Raycaster(new THREE.Vector3(), new THREE.Vector3(0, -1, 0), 0, 10);
+
+    // End First Person Controls
+
+    // Add world geometry
+
+    // text
+
+    // materials for the text
+    materials = [
+        new THREE.MeshPhongMaterial({ color: 0x10b10c, flatShading: true }), // front
+        new THREE.MeshPhongMaterial({ color: 0x0c9909 }) // side
+    ];
+
+    // establish font loader
+    const loader = new TTFLoader();
+
+    // use loader with desired ttf font
+    loader.load("../assets/CourierPrime-Bold.ttf", function (json) {
+        font = new Font(json);
+        // see create text function below 
+        createText();
+    });
+
+    // add resulting shapes to scene
+    group = new THREE.Group();
+    group.position.y = 100;
+
+    scene.add(group);
+
+    // video
+
+    // Video is included in the HTML but is not visible.
+    // look at the index.html in this folder. 
+    //
+    // Added a play command to the event listener staring on line 70
+
+    // load video from HTML and apply to texture
+    video = document.getElementById("video");
+    video.addEventListener("play", function () {
+        this.currentTime = 0;
+    });
+    vidTexture = new THREE.VideoTexture(video);
+    vidTexture.colorSpace = THREE.SRGBColorSpace;
+    const vidMaterial = new THREE.MeshBasicMaterial({ map: vidTexture });
+
+    // create video screen shape
+    const vidGeometry = new THREE.PlaneGeometry(170.8, 96);
+    // apply image to shape and add to scene
+    const vidPlane = new THREE.Mesh(vidGeometry, vidMaterial);
+    vidPlane.position.set(0, 0, -100);
+    scene.add(vidPlane);
+    
+    // Sample Objects
+    
+    const spatialObject = new THREE.TorusKnotGeometry(10, 3, 100, 16);
+    const flatObject = new THREE.PlaneGeometry(50,100);
+    
+    // Sample Materials
+    
+
+    // Ground
+    const earth = new THREE.PlaneGeometry(4000, 4000);
+    const ground = new THREE.MeshPhongMaterial({ color: 0xe10dee, flatShading: true });
+    const mesh2 = new THREE.InstancedMesh(earth, ground, 500);
+    mesh2.translateY(-60);
+    mesh2.rotateX(-1.5708);
+    scene.add(mesh2);
+
+    // lights
+    const dirLight1 = new THREE.DirectionalLight(0xffffff, 3);
+    dirLight1.position.set(1, 1, 1);
+    scene.add(dirLight1);
+
+    const dirLight2 = new THREE.DirectionalLight(0xffffff, 2);
+    dirLight2.position.set(-1, -1, -1);
+    scene.add(dirLight2);
+}
+
+// Function to update moving objects, in this case the camera.
+// The render function is trigger at the end to update the canvas.
+function animate() {
+    // Start First Person Control Animations
+    const time = performance.now();
+    if (controls.isLocked === true) {
+        const delta = (time - prevTime) / 1000;
+
+        velocity.x -= velocity.x * 10.0 * delta;
+        velocity.z -= velocity.z * 10.0 * delta;
+
+        velocity.y -= 9.8 * 100.0 * delta; // 100.0 = mass
+
+        direction.z = Number(moveForward) - Number(moveBackward);
+        direction.x = Number(moveRight) - Number(moveLeft);
+        direction.normalize(); // this ensures consistent movements in all directions
+
+        if (moveForward || moveBackward) velocity.z -= direction.z * 400.0 * delta;
+        if (moveLeft || moveRight) velocity.x -= direction.x * 400.0 * delta;
+
+        controls.moveRight(-velocity.x * delta);
+        controls.moveForward(-velocity.z * delta);
+
+        // jump fix
+        controls.object.position.y += velocity.y * delta;
+        if (controls.object.position.y < 10) {
+            velocity.y = 0;
+            controls.object.position.y = 10;
+
+            canJump = true;
+        }
+    }
+
+    prevTime = time;
+    // End First Person Control Animations
+
+    render();
+}
+
+// Function to render the scene using the camera.
+function render() {
+    renderer.render(scene, camera);
+}
+
+// Function to generate text shapes
+function createText() {
+    // create geomtery with parameters, change parameters to test modifications
+    // "text" on next line is the message to be written
+    textGeo = new TextGeometry(text, {
+        font: font,
+        size: 5,
+        depth: 2,
+        curveSegments: 4,
+        bevelThickness: 2,
+        bevelSize: 1.5,
+        bevelEnabled: true
+    });
+
+    // finish making geometry
+    textGeo.computeBoundingBox();
+    const centerOffset = -0.5 * (textGeo.boundingBox.max.x - textGeo.boundingBox.min.x);
+
+    // apply material to geometry
+    textMesh1 = new THREE.Mesh(textGeo, materials);
+
+    // set position and rotation
+    textMesh1.position.x = centerOffset;
+    textMesh1.position.z = -100;
+    textMesh1.position.y = -50;
+    textMesh1.rotation.x = 0;
+    textMesh1.rotation.y = Math.PI * 2;
+
+    // add to group to be added to scene
+    group.add(textMesh1);
+}
