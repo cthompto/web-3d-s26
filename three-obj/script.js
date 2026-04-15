3da8d3209ecd6b0c55a3be6d2b84b3eb62bd099a
Chelsea Thompto <chelsea.l.thompto@gmail.com>
2026-04-15 08:59:17 -0400

    update example

--- three-obj/script.js
+++ three-obj/script.js
@@ -1,5 +1,10 @@
+// three.js website demo slightly modified
+
+// import main library and loaders
+
 import * as THREE from "three";
 
+// be sure loaders are in your "src" or "addons" folder
 import { MTLLoader } from "../src/MTLLoader.js";
 import { OBJLoader } from "../src/OBJLoader.js";
 import { OrbitControls } from "../src/OrbitControls.js";
@@ -8,6 +13,7 @@ let camera, scene, renderer, controls;
 
 init();
 
+// this uses an asynchronous function because of the file size
 async function init() {
     camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 20);
     camera.position.z = 2.5;
@@ -23,17 +29,32 @@ async function init() {
     camera.add(pointLight);
     scene.add(camera);
 
-    // model
+    // wire material
+    const wireMat = new THREE.MeshBasicMaterial({
+        color: 0x7a02fd,
+        wireframe: true
+    });
 
+    // model loading
+
+    // loading texture
+
+    // MTL file loading
+    // this sets the path to where the file is
     const mtlLoader = new MTLLoader().setPath("../assets/obj_mtl_ex/");
+    // this loads the model
     const materials = await mtlLoader.loadAsync("male02.mtl");
     materials.preload();
 
+    // OBJ loading
+    // this sets the path to where the file is
     const objLoader = new OBJLoader().setPath("../assets/obj_mtl_ex/");
+    // this adds MTL file to the model if you have it
     objLoader.setMaterials(materials); // optional since OBJ assets can be loaded without an accompanying MTL file
-
+    // this loads the model
     const object = await objLoader.loadAsync("male02.obj");
 
+    // position scale and add model to scene
     object.position.y = -0.95;
     object.scale.setScalar(0.01);
     scene.add(object);
